66b646df7241a58619921035d295c0ae8a21e49d
JoushuaDelaCruz <joushuadelacruz@gmail.com>
2023-12-07 17:19:25 -0800

    Changed corsOptions

--- configs/corsConfig.js
+++ configs/corsConfig.js
@@ -1,14 +1,5 @@
 export const corsOptions = {
+  origin: "*",
   credentials: true,
-  exposedHeaders: ["Content-Length", "Content-Encoding"],
-  allowedHeaders: [
-    "Accept",
-    "X-Access-Token",
-    "X-Application-Name",
-    "X-Request-Sent-Time",
-    "Content-Type",
-  ],
-  methods: ["GET", "POST", "OPTIONS"],
-  origin: "https://shimmering-lily-0f8e29.netlify.app",
-  secure: true,
+  optionSuccessStatus: 200,
 };
